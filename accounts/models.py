ab697f37cac879bc644d58e8c518bf035d908fdc
Priyanshu Verma <201652019@iiitvadodara.ac.in>
2019-07-10 08:24:28 +0530

    modified views.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib import auth
 # Create your models here.
+
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def __str__(self):
+        return "@{}".format(self.username)
--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from . import forms
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
