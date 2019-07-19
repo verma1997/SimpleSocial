0ffe193a601cc7fadeeb51250c4f8cfb82b43d8e
Priyanshu Verma <201652019@iiitvadodara.ac.in>
2019-07-19 14:36:40 +0530

    added group files

--- groups/views.py
+++ groups/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.core.urlresolvers import reverse
+from django.views import generic
 # Create your views here.
+from groups.models import Group, GroupMember
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields = ('name','description')
+    model = Group
+
+class SingleGroup(genric.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
